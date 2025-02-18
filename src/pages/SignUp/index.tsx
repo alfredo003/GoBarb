281544d7ee6e04054c71c0e482a0044f0881963b
alfredo chivela <alfredochivela3@gmail.com>
2025-02-18 22:35:21 +0100

    update

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,22 +1,27 @@
 import { Container ,Content,Background} from "./styles";
 import logoimg from "../../assets/logo.svg"
 import { FiLock, FiArrowLeft, FiMail, FiUser } from "react-icons/fi";
+import { Form} from "@unform/web";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const SignUp:React.FC = () =>{
+    const handleSubmit = (data: object) => {
+        console.log(data);
+    };
+
     return (
         <Container>         
              <Background />
           <Content>
             <img src={logoimg} alt="GoBarber" />
-            <form action="" method="post">
+            <Form initialData={{name:"angola"}} onSubmit={handleSubmit}>
               <h1>Faça seu Cadastro</h1>
-              <Input type="text" icon={FiUser} name="" placeholder="Nome" />
-              <Input type="text" icon={FiMail} name="" placeholder="Email" />
-              <Input type="password"icon={FiLock} name="" placeholder="Senha" />
-              <Button>Cadastrar</Button>
-            </form>
+              <Input type="text" icon={FiUser} name="name" placeholder="Nome" />
+              <Input type="text" icon={FiMail} name="email" placeholder="Email" />
+              <Input type="password"icon={FiLock} name="password" placeholder="Senha" />
+              <Button type="submit">Cadastrar</Button>
+            </Form>
             <a href="">
               <FiArrowLeft/>
               Voltar para o login</a>
